ddb3ae030c41a6f069c348a95ee436afa2690f26
Francisco <franciscogaletti@yahoo.com>
2021-05-10 01:57:04 -0500

    Added address

--- AzureTable.Project/Customer.cs
+++ AzureTable.Project/Customer.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 namespace AzureTable.Project
 {
-    class Customer
+    public  class Customer
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public int Id { get; set; }  
+
+        public string Name { get; set; }  
     }
 }
