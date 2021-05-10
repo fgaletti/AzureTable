08249facba53f7136b66c246824c63704f93aa3a
Francisco <franciscogaletti@yahoo.com>
2021-05-10 02:37:24 -0500

    Adde Phone

--- AzureTable.Project/Customer.cs
+++ AzureTable.Project/Customer.cs
@@ -11,6 +11,7 @@ namespace AzureTable.Project
         public string LastName { get; set; }
         public int CityId { get; set; }
         public string State { get; set; }
+        public string Phone { get; set; }
 
     }
 }
